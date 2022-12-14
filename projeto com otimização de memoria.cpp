9b2a1aca1d7299c787db5f5d50e53d2f54018a7b
Luciano Pereira <54913266+luci18530@users.noreply.github.com>
2022-12-14 14:10:03 -0300

    Add files via upload

--- projeto com otimização de memoria.cpp
+++ projeto com otimização de memoria.cpp
@@ -16,26 +16,27 @@ double step;
 using namespace std;
 
 int main () {
-    double a[NUM], b[NUM], c[NUM], d[NUM];
-    int i,j;
+    //double a[NUM], b[NUM], c[NUM], d[NUM];
+    int effarray[NUM][4];
+    int i,j, temp;
     auto start = std::chrono::high_resolution_clock::now();
 
     for (i=0; i < NUM; i++) {
-    auto values = std::make_tuple(i * 1.0, i * 2.0, i * 3.0, i * 4.0);
-    a[i] = std::get<0>(values);
-    b[i] = std::get<1>(values);
-    c[i] = std::get<2>(values);
-    d[i] = std::get<3>(values);
+        temp = i * 1;
+        effarray[i][0] = temp;
+        effarray[i][1] = temp * 2;
+        effarray[i][2] = temp * 3;
+        effarray[i][3] = temp * 4;
     }
 
-    for (i=0; i < NUM-2; i++) {
-        for (j=0; j < NUM-2; j++) {
-            a[i] = b[i] + c[i] * d[i];
+    for (i=0; i < NUM; i++) {
+        for (j=0; j < NUM; j++) {
+            effarray[i][0] = effarray[i][1] + effarray[i][2];
         }
     }
 
     for (i=0; i < NUM; i++) {
-        d[i] = a[i] * a[i];
+        effarray[i][3] = effarray[i][0] * effarray[i][0];
     }
 
     auto finish = std::chrono::high_resolution_clock::now();
