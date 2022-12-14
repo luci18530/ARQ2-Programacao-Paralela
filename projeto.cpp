51e5554d59a825eddab9cc31e8b2f81dbe218424
Luciano Pereira <54913266+luci18530@users.noreply.github.com>
2022-12-14 09:30:55 -0300

    código inicial

---
+++ projeto.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <ctime>
+#include <math.h>
+#include <chrono>
+#include <iostream>
+
+// GRUPO : LUCIANO PEREIRA, GIOVANNA ALBURQUERQUE, RYAN MATHEUS
+
+static long NUM = 100000;
+double step;
+
+using namespace std;
+
+int main () {
+    double a[NUM], b[NUM], c[NUM], d[NUM];
+    int i,j;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (i=0; i < NUM; i++) {
+        a[i] = i * 1.0;
+        b[i] = i * 2.0;
+        c[i] = i * 3.0;
+        d[i] = i * 4.0;
+    }
+
+    for (i=0; i < NUM-2; i++) {
+        for (j=0; j < NUM-2; j++) {
+            a[i] = b[i] + c[i] * d[i];
+        }
+    }
+
+    for (i=0; i < NUM; i++) {
+        d[i] = a[i] * a[i];
+    }
+
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
+}
